a96ac187ffbb491330f1cf25a45fe28cd6e90422
root <saravanaoff2000@gmail.com>
2021-07-01 17:30:27 +0530

    Initial Commit

---
+++ static/src/js/dev_editor.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.banner .menu-btn').click(function(){
+      $('.tonavbar .menu').toggleClass("active");
+      $('.banner .menu-btn i').toggleClass("active");
+  })
+  })
+
